01289d2285510efc1e96e76ec6e9d9b90cc3543b
Ivan Gaudino <s257971@studenti.polito.it>
2022-05-09 23:45:39 +0200

    Primo esercizio simulatori ad eventi discreti

---
+++ src/main/java/it/polito/tdp/noleggio/model/TestSimulatore.java
@@ -0,0 +1,13 @@
+package it.polito.tdp.noleggio.model;
+
+public class TestSimulatore {
+
+	public static void main(String[] args) {
+		Simulatore sim = new Simulatore(18);
+		sim.caricaEventi();
+		sim.run();
+		System.out.println("Clienti: "+sim.getnClientiTot()+" di cui "+
+		sim.getnClientiInsoddisfatti()+" insoddisfatti\n");
+	}
+
+}
